caa6e10d79710b1dc4432ddddf44a97c5d1d7a20
Justin Rissler <justin@goparagon.com>
2023-06-15 12:56:13 -0400

    Add custom color background based on data

--- TestDateEditCellCustomization/ViewModels/MainViewModel.cs
+++ TestDateEditCellCustomization/ViewModels/MainViewModel.cs
@@ -1,4 +1,10 @@
 ﻿using DevExpress.Mvvm.CodeGenerators;
+using DevExpress.Xpf.Editors.DateNavigator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Documents;
+using System.Windows.Media;
 
 namespace TestDateEditCellCustomization.ViewModels
 {
@@ -6,12 +12,51 @@ namespace TestDateEditCellCustomization.ViewModels
     public partial class MainViewModel
     {
         [GenerateProperty]
-        string _Status;
+        // this could be a property of all dates with notes, to have them appear in bold.
+        List<DateTime> _Dates;
+
         [GenerateProperty]
-        string _UserName;
+        List<TblDateNote> _DateNotes;
+
+        public MainViewModel()
+        {
+            #region Get test data
+            Dates = new List<DateTime>()
+            {
+                DateTime.Today.AddDays(2),
+                DateTime.Today.AddDays(4),
+                DateTime.Today.AddDays(6),
+                DateTime.Today.AddDays(8),
+            };
+
+            DateNotes = new List<TblDateNote>();
+
+            for (int i = 0; i < Dates.Count; i++)
+            {
+                var date = Dates[i];
+                DateNotes.Add(new TblDateNote() { Background = i % 2 == 0 ? "#fc9595" : "#959ffc", Date = date, Note = i % 2 == 0 ? "Red note" : "Blue note" });
+            }
+            #endregion
+        }
 
         [GenerateCommand]
-        void Login() => Status = "User: " + UserName;
-        bool CanLogin() => !string.IsNullOrEmpty(UserName);
+        public void RequestCellAppearance(DateNavigatorRequestCellAppearanceEventArgs e)
+        {
+            var dayNote = DateNotes.FirstOrDefault(w => w.Date == e.DateTime);
+
+            if (dayNote != null)
+            {
+                var converter = new BrushConverter();
+                var brush = (Brush)converter.ConvertFromString(dayNote.Background);
+                e.Appearance.Background = brush;
+            }
+        }
+    }
+
+    public class TblDateNote
+    {
+        public DateTime Date { get; set; }
+        public string Background { get; set; }
+        public string Note { get; set; }
     }
 }
